c39b8baeebf3def2ffb49b2979a5207d392280d3
kendall <motesmass@gmail.com>
2017-09-07 12:06:48 -0600

    finally setup

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -11,7 +11,10 @@
 #include <cstdlib>
 
 int main(int argc, char * argv[]) {
+    
+    
 
+    
     // Load GLFW and Create a Window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -20,6 +23,8 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+    
+    
 
     // Check for Valid Context
     if (mWindow == nullptr) {
@@ -35,8 +40,44 @@ int main(int argc, char * argv[]) {
 	// **************************************
 	// Setup Vertex arrays here
 	// **************************************
+    
+    //load shader
+    GLuint myShader = LoadProgram("/Users/kendall/Glitter/Glitter/Shaders/basic.vert", "/Users/kendall/Glitter/Glitter/Shaders/basic.frag");
+    
 
-	// **************************************
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+    
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    //add vertex array
+    unsigned int vaoID[1]; // Our Vertex Array Object
+    glGenVertexArrays(1, &vaoID[0]); // Create our Vertex Array Object
+    //bind vertex array
+    glBindVertexArray(vaoID[0]); // Bind our Vertex Array Object so we can use it
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(
+                          0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                          3,                  // size
+                          GL_FLOAT,           // type
+                          GL_FALSE,           // normalized?
+                          3*sizeof(GLfloat),                  // stride
+                          (void*)0            // array buffer offset
+                          );
+    // Draw the triangle !
+   
 	
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
@@ -49,8 +90,14 @@ int main(int argc, char * argv[]) {
 
 		// **********************************
 		// Add rendering code here
-
-		// **********************************
+        
+        
+        //get my vertexarray and bind it
+        glUseProgram(myShader);
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+       
+        
+        // **********************************
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
